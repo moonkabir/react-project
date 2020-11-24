a524934f05580ccc78259466b04c923b94878ef0
MoonKabir <45933210+moonkabir@users.noreply.github.com>
2020-11-25 00:37:24 +0600

    Add files via upload

---
+++ loginsignup.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+export class LoginSignup extends Component {
+    state = {
+        isSignup:false
+    }
+    showSignupForm = ()=>{
+        this.setState({isSignup:true})
+    }
+    showLogin = ()=>{
+        this.setState({isSignup:false})
+    }
+    render() {
+        return (
+            <div style = {styles.container}>
+                <div className="loginsignup">
+                    <input style={{...styles.input,display:this.state.isSignup?'block':'none'} } type = "text" name="fullname" placeholder="full name" />
+                    <input style={styles.input} type = "email" name="email" placeholder="Email Address" />
+                    <input style={styles.input} type = "password" name="password" placeholder="Enter password" />
+                </div>
+                <div style={{display:this.state.isSignup?'none':'block'}} onClick={this.showSignupForm}>Sign Up Now</div>
+                <div style={{display:this.state.isSignup?'block':'none'}} onClick={this.showLogin}>Go Bck To Login</div>
+            </div>
+        )
+    }
+}
+
+export default LoginSignup
+
+
+const styles = {
+    input:{
+        fontSize:18,
+        color:'blue',
+        display:'block',
+        margin:20
+    }
+}
